127c9686e2d29e8eac7748807627269a53fe0c19
jmjmjmjmjmjm <wlsauddlv@naver.com>
2020-12-08 17:50:49 +0900

    2020-12-08

--- src/jumpking/Stage.java
+++ src/jumpking/Stage.java
@@ -25,30 +25,30 @@ public class Stage extends JPanel {
 	ImageIcon bgs = new ImageIcon("점프킹이미지/맵/map1.jpg");
 	private Image imgs = bgs.getImage();
 	int y = 3030;
-	int yy = 2024;
+	int yy = 1824;
 
 	protected void paintComponent(Graphics g) {
 		g.drawImage(imgs, 0, 0, 1800, 900, 230, yy, 1800, y, this);
 		repaint();
 	}
 
-	public Stage() {
-		setFocusable(true);
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				while (true) {
-					y--;
-					yy--;
-					repaint();
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+//	public Stage() {
+//		setFocusable(true);
+//		new Thread(new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				while (true) {
+//					y--;
+//					yy--;
+//					repaint();
+//					try {
+//						Thread.sleep(10);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//		}).start();
+//	}
 }
--- src/jumpking/점프어플.java
+++ src/jumpking/점프어플.java
@@ -7,8 +7,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import jumpking.BubbleApp.BgJumpKing;
-
 public class 점프어플 extends JFrame {
 
 	private 점프어플 jumpApp = this;
@@ -27,8 +25,8 @@ public class 점프어플 extends JFrame {
 	private ImageIcon icJumpL1, icJumpL2, icJumpL3, icJumpL4; // 좌 점프 이미지
 
 	public 점프어플() {
-		init(); // new 오브젝트를
 		setting(); // JFrame 기본세팅
+		init(); // new 오브젝트를
 		batch(); // 화면 구성
 		listener(); // 리스너(이벤트)
 		setVisible(true);
@@ -38,62 +36,51 @@ public class 점프어플 extends JFrame {
 		setTitle("Jump King");
 		setSize(1620, 1000);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setContentPane(panel); // 투명도
-		//setContentPane(stage); //실제 화면보여지는것
+		//setContentPane(panel); // 투명도
+		 setContentPane(stage); //실제 화면보여지는것
 		setLayout(null);
 		setExtendedState(JFrame.MAXIMIZED_BOTH); // 전체화면 모드
 	}
 
 	public void init() { // 오브젝트들
-		bitMap = "점프킹이미지/맵/투명도.jpg"; // 실제 연산되는 맵
 		player = new Player();
-		thPixel = new Thread(new PixelCheck(player, bitMap)); // 색깔 연산 스레드
-		thPixel.start();
-		
+
 	}
 
 	public void batch() { // 화면구성
 		add(player);
-
 	}
 
-	public void listener() { // 리스너 (부딫히는거)
+	public void listener() { // 리스너
 		addKeyListener(new KeyAdapter() {
 
-			@Override
 			public void keyPressed(KeyEvent e) {
-
-				if (player.isMoveLock() == true) { // Move락이 걸려있으면 메서드실행안됨
-					return;
-				}
-
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) { // 오른쪽 이동
-					player.setJumpUpDirectionStay(true); // 제자리 점프시 우측 방향 설정 (우측 이미지 때문에 사용)
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 					player.moveRight();
-//					System.out.println(player.getPlayerX() +" a "+ player.getPlayerY());
-				} else if (e.getKeyCode() == KeyEvent.VK_LEFT) { // 왼쪽이동
-					player.setJumpUpDirectionStay(false); // 제자리 점프시 좌측 방향 설정 (좌측 이미지 때문에 사용)
-					player.moveLeft();
-				} else if (e.getKeyCode() == KeyEvent.VK_UP && player.isLeft() == true) { // 좌측 위 점프
-					player.setJumpUpDirection(-1);// 좌측 방향값 설정
-					player.jumpUp();
-				} else if (e.getKeyCode() == KeyEvent.VK_UP && player.isRight() == true) { // 우측 위 점프
-					player.setJumpUpDirection(1); // 우측 방향값 설정
-					player.jumpUp();
-				} else if (e.getKeyCode() == KeyEvent.VK_UP) {// 제자리 점프
-					player.jumpUp();
 				}
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					player.moveLeft();
+				}
+				if (e.getKeyCode()==KeyEvent.VK_UP) {
+					player.moveUp();
+				}
+
 			}
 
 			@Override
-			public void keyReleased(KeyEvent e) { // 버튼땔때 멈추는 함수
-				if (e.getKeyCode() == KeyEvent.VK_UP) // UP버튼 연속입력 방지
-					player.setUp(false);
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) // 좌,우이동 매끄러운 변환 조건문
-					player.setRight(false);
-				else if (e.getKeyCode() == KeyEvent.VK_LEFT)
-					player.setLeft(false);
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					player.isRight = false;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					player.isLeft = false;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
+					player.isUp = false;
+				}
+
 			}
+
 		});
 	}
 
